31e502f495203ccc024bec5ad63118c56fb4404c
Jędrzej <34661811+Jwasik@users.noreply.github.com>
2021-03-13 12:44:04 +0100

    Initial commit

---
+++ Battleships/Battleships.cpp
@@ -0,0 +1,29 @@
+﻿#include <SFML/Graphics.hpp>
+
+int main()
+{
+    sf::RenderWindow window(sf::VideoMode(1024, 768), "Battleships");
+
+    sf::Text text;
+    text.setCharacterSize(20);
+    sf::Font sm;
+    sm.loadFromFile("SM.TTF");
+    text.setFont(sm);
+    text.setString("Battleships");
+
+    while (window.isOpen())
+    {
+        sf::Event event;
+        while (window.pollEvent(event))
+        {
+            if (event.type == sf::Event::Closed)
+                window.close();
+        }
+
+        window.clear();
+        window.draw(text);
+        window.display();
+    }
+
+    return 0;
+}
