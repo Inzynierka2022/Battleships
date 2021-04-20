256b7b9192e902724dcb6aa73f8d5ea195cbe62d
Jędrzej <34661811+Jwasik@users.noreply.github.com>
2021-04-20 15:22:33 +0200

    Revert "Merge branch 'menu' into gameEngine"
    This reverts commit 35432b3453b967b5a20615f74e4aace0bc1cd2b8, reversing
    changes made to 2ceead88e72d59d435067867a454110bb70d867d.

--- Battleships/Button.cpp
+++ Battleships/Button.cpp
@@ -1,12 +1,11 @@
 #include "Button.h"
 
 sf::Vector2f Button::rectangleSize = sf::Vector2f(350, 55);
+std::string Button::fontFile = "SM.TTF";
 sf::Color Button::rectangleOutlineColor = sf::Color(50, 50, 100, 200);
 sf::Color Button::rectangleColor = sf::Color(0,0,255,128);
-int8_t Button::rectangleOutlineThickness = -5;
-std::string Button::fontFile = "SM.TTF";
 sf::Color Button::textColor = sf::Color::White;
-
+int8_t Button::rectangleOutlineThickness = -5;
 
 
 Button::Button(const sf::Vector2f& position, std::string content)
@@ -93,16 +92,8 @@ void Button::setSize(const sf::Vector2f &s)
 	this->calculateOrigin();
 }
 
-void Button::setCanHover(bool b)
-{
-	this->canHover = b;
-}
-
-
-
 void Button::hover()
 {
-	if (!canHover) return;
 	this->invertColors();
 }
 
@@ -119,6 +110,11 @@ sf::FloatRect Button::getLocalBounds()
 sf::FloatRect Button::getGlobalBounds()
 {
 	return this->rectangle.getGlobalBounds();
+}
+
+void Button::setString(const std::string& string)
+{
+	this->text.setString(string);
 }
 
 void Button::setPosition(const sf::Vector2f& pos)
